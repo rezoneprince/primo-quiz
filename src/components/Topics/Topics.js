4f043aae66aabd1e65eb1eca1410a0c131984fde
rezoneprince <rezoneprinceme@gmail.com>
2022-10-11 23:37:59 +0600

    topics update done

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -4,7 +4,11 @@ const Topics = () => {
     const { data } = useLoaderData();
     return (
         <div className=" container mx-auto p-10 ">
-
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                {data.map((topic) => (
+                    <Topic key={topic.id} topic={topic}></Topic>
+                ))}
+            </div>
         </div>
     );
 };
