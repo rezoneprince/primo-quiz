ed288d9e14fd069f22291d30b036e4a187da83b8
rezoneprince <rezoneprinceme@gmail.com>
2022-10-12 12:08:08 +0600

    topic work add

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,40 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <nav className="container mx-auto p-5 bg-slate-200">
-            <div className="flex justify-between items-center">
-                <div className="">
-                    <Link to="/">
-                        <h1 className="text-2xl font-bold">
-                            Primo <span className="text-orange-500">Quiz</span>
-                        </h1>
-                    </Link>
-                </div>
-                <div className="flex gap-5">
-                    <Link
-                        className="text-xl hover:text-orange-500 font-semibold"
-                        to="/topic"
-                    >
-                        Topic
-                    </Link>
-                    <Link
-                        className="text-xl hover:text-orange-500 font-semibold"
-                        to="/statistics"
-                    >
-                        Statistics
-                    </Link>
-                    <Link
-                        className="text-xl hover:text-orange-500 font-semibold"
-                        to="/blog"
-                    >
-                        Blog
-                    </Link>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="container mx-auto p-5 shadow-md bg-slate-200">
+      <div className="flex justify-between items-center">
+        <div className="">
+          <Link to="/">
+            <h1 className="text-2xl font-bold">
+              Primo <span className="text-orange-500">Quiz</span>
+            </h1>
+          </Link>
+        </div>
+        <div className="flex gap-5">
+          <NavLink
+            className="text-xl hover:text-orange-500 font-semibold"
+            to="/topic"
+          >
+            Topic
+          </NavLink>
+          <NavLink
+            className="text-xl hover:text-orange-500 font-semibold"
+            to="/statistics"
+          >
+            Statistics
+          </NavLink>
+          <NavLink
+            className="text-xl hover:text-orange-500 font-semibold"
+            to="/blog"
+          >
+            Blog
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
